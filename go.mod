d1334ab304ab1314d24a6b584b9c3c4d48c5df35
fabrizioguespe <fabrizioguespe@MacFabri.local>
2023-06-12 20:59:42 -0300

    new

---
+++ go.mod
@@ -0,0 +1,18 @@
+module connect-web
+
+go 1.20
+
+require (
+	github.com/envoyproxy/protoc-gen-validate v1.0.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/iancoleman/strcase v0.2.0 // indirect
+	github.com/lyft/protoc-gen-star/v2 v2.0.3 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.9.3 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getToken, getMessaging, onMessage } from "firebase/messaging";
 
+import { Notifications } from "./gen/service_connectweb.ts";
+import { createPromiseClient } from "@bufbuild/connect";
+import { createConnectTransport } from "@bufbuild/connect-web";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC6zKD5-mGA5agzgv2oyQfCihzsoZ9t9Z8",
   authDomain: "xmtp-web.firebaseapp.com",
@@ -11,7 +15,7 @@ const firebaseConfig = {
   measurementId: "G-3G4XJ8LW4H",
 };
 
-console.log("*** Firebase Config ***", firebaseConfig);
+//console.log("*** Firebase Config ***", firebaseConfig);
 
 const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
@@ -36,11 +40,46 @@ export const getOrRegisterServiceWorker = () => {
 export const getFirebaseToken = () =>
   getOrRegisterServiceWorker().then((serviceWorkerRegistration) =>
     getToken(messaging, {
-      vapidKey:
-        "BGiVz5g2sQwFUBsKc0rtgGkavtQX7wfsf5XOlp56UXaOmeywlMj7i-5wGfBRZurLUyrLHOimyVJCHY2aGsVfLWU",
+      vapidKey: process.env.REACT_APP_VAPID_KEY,
       serviceWorkerRegistration,
     })
   );
+
+async function register() {
+  const transport = createConnectTransport({
+    baseUrl:
+      "http://localhost:8080/" || "https://notifications.dev.xmtp.network/",
+  }); // Here we make the client itself, combining the service
+  // definition with the transport.
+  const client = createPromiseClient(Notifications, transport);
+  console.log("client", client);
+
+  const deviceToken = await getFirebaseToken();
+  console.log("deviceToken", deviceToken);
+  const installationId = deviceToken; // Use the device token as installationId for simplicity
+
+  console.log("jeje", {
+    installationId,
+    deliveryMechanism: {
+      deliveryMechanismType: {
+        value: deviceToken,
+        case: "firebaseDeviceToken",
+      },
+    },
+  });
+  await client.registerInstallation(
+    {
+      installationId,
+      deliveryMechanism: {
+        deliveryMechanismType: {
+          value: deviceToken,
+          case: "firebaseDeviceToken",
+        },
+      },
+    },
+    {}
+  );
+}
 
 export const onForegroundMessage = () =>
   new Promise((resolve) => onMessage(messaging, (payload) => resolve(payload)));
